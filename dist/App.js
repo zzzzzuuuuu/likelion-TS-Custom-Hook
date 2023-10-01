9d53e4988ce8f47859e64c648edb82d92c65574f
yhyem <yoonhyemin0120@gmail.com>
2023-10-01 23:53:30 +0900

    setting: tsconfig 파일 추가

---
+++ dist/App.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const logo_svg_1 = __importDefault(require("./logo.svg"));
+require("./App.css");
+function App() {
+    return className = "App" >
+        className;
+    "App-header" >
+        src;
+    {
+        logo_svg_1.default;
+    }
+    className = "App-logo";
+    alt = "logo" /  >
+        Edit < code > src / App.js < /code> and save to reload.
+        < /p>
+        < a;
+    className = "App-link";
+    href = "https://reactjs.org";
+    target = "_blank";
+    rel = "noopener noreferrer"
+        >
+            Learn;
+    React
+        < /a>
+        < /header>
+        < /div>;
+    ;
+}
+exports.default = App;
