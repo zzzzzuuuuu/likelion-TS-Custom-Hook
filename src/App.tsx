e622834bca62ee9921905eb11d1eafef90cc00bb
yhyem <yoonhyemin0120@gmail.com>
2023-11-03 00:05:01 +0900

    setting: ts 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>커밋 테스트용</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
